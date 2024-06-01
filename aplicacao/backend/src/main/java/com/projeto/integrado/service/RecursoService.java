e5151f02071bd30ba14a288ea9e20762798531cc
willi <wil.liam_benicio@hotmail.com>
2024-06-01 12:44:51 -0300

    Initial commit

---
+++ aplicacao/backend/src/main/java/com/projeto/integrado/service/RecursoService.java
@@ -0,0 +1,56 @@
+package com.projeto.integrado.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.projeto.integrado.entity.Projeto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.projeto.integrado.entity.Recurso;
+import com.projeto.integrado.repository.RecursoRepository;
+
+@Service
+public class RecursoService {
+	@Autowired
+	RecursoRepository recursoRepository;
+	
+	public List<Recurso> getAll(){
+		return recursoRepository.findAll();
+	}
+	
+	public Recurso getById(Integer id) {
+		return recursoRepository.findById(id).orElse(null) ;
+	}
+	
+	public Recurso saveRecurso(Recurso recurso) {
+		return recursoRepository.save(recurso);
+	}
+
+	public Optional<Recurso> buscarProjeto(Recurso recurso) {
+
+		var existeRecurso = recursoRepository.findByRecursoNome(recurso.getRecursoNome());
+
+		return existeRecurso;
+	}
+	
+	public Recurso updateRecurso(Integer id, Recurso recurso) {
+		Recurso recursoAtualizado = recursoRepository.findById(id).orElse(null);
+		if(recursoAtualizado != null) {
+			recursoAtualizado.setRecursoFuncao(recurso.getRecursoFuncao());
+			recursoAtualizado.setRecursoNome(recurso.getRecursoNome());
+			return recursoRepository.save(recursoAtualizado);
+		}else {
+			return null;
+		}
+	}
+
+	public Boolean deleteRecurso(Integer id) {
+		Recurso recurso = recursoRepository.findById(id).orElse(null);
+		if(recurso != null) {
+			recursoRepository.delete(recurso);
+			return true;
+		}else {
+			return false;
+		}
+	}
+}
